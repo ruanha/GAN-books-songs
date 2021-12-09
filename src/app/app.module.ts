7cbbcd67ce4351e78f4b23a773e8ffb23e1d0939
Rune <rune_hartvig@hotmail.com>
2021-12-09 21:16:00 +0100

    move all components into modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MaterialModule } from './material.module';
 import { HttpClientModule } from '@angular/common/http';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { SearchBarComponent } from './components/search-bar/search-bar.component';
-import { NavBarComponent } from './components/nav-bar/nav-bar.component';
-import { CardComponent } from './components/card/card.component';
-import { FrontpageComponent } from './components/frontpage/frontpage.component';
-import { ListComponent } from './components/list/list.component';
-import { SongsPageComponent } from './components/songs-page/songs-page.component';
-import { BooksPageComponent } from './components/books-page/books-page.component';
+
+import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
+import { BooksModule } from './books/books.module';
+import { SongsModule } from './songs/songs.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchBarComponent,
-    NavBarComponent,
-    CardComponent,
-    FrontpageComponent,
-    ListComponent,
-    SongsPageComponent,
-    BooksPageComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +23,11 @@ import { BooksPageComponent } from './components/books-page/books-page.component
     MaterialModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule,
+    CoreModule,
+    BooksModule,
+    SongsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
